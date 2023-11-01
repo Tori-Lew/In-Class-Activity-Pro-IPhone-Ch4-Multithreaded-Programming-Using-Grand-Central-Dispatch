adfffb2ddae36bf266ab991ab67efb7dfeb250fd
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-01 16:35:42 -0700

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch4 Multithreaded Programming Using Grand Central Dispatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch4 Multithreaded Programming Using Grand Central Dispatch
+//
+//  Created by Student Account on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch4 Multithreaded Programming Using Grand Central Dispatch/In_Class_Activity_Pro_IPhone_Ch4_Multithreaded_Programming_Using_Grand_Central_DispatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch4_Multithreaded_Programming_Using_Grand_Central_DispatchApp.swift
+//  In-Class Activity Pro IPhone Ch4 Multithreaded Programming Using Grand Central Dispatch
+//
+//  Created by Student Account on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch4_Multithreaded_Programming_Using_Grand_Central_DispatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
